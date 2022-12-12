a805a90b55dbd77a9184ad6e6e18a5249c2893e3
Luc Weishar <71008362+Luc-Wshr@users.noreply.github.com>
2022-12-12 17:36:15 +0100

    Update Spark.py

--- Spark.py
+++ Spark.py
@@ -28,3 +28,29 @@ lines = test.select(split(test.value, " ").alias("Zeile"))
 lines.show(10, truncate=100)
 # das Schema: das Split hat ein Array generiert
 lines.printSchema()
+
+
+
+# Unzip Zip-Files
+import zipfile
+import pyspark.sql.functions as F
+
+# specify the zip file name
+zip_file_name = "202211-citibike-tripdata.csv.zip"
+
+# create a ZipFile object
+zip_file = zipfile.ZipFile(zip_file_name)
+
+# extract all files from the zip file
+# the extractall() method will extract all the files to the current directory
+zip_file.extractall()
+
+# close the ZipFile object
+zip_file.close()
+
+
+
+# CSV Daten auslesen
+
+citybike = spark.read.csv("202211-citibike-tripdata.csv",header = True)
+citybike.show(truncate=False)
