9f1b9233bcfb48c93173b40d6027245d25d69309
DominikScheiwein <72744636+DominikScheiwein@users.noreply.github.com>
2022-12-12 17:37:55 +0100

    Update Spark.py

--- Spark.py
+++ Spark.py
@@ -31,22 +31,7 @@ lines.printSchema()
 
 
 
-# Unzip Zip-Files
-import zipfile
-import pyspark.sql.functions as F
 
-# specify the zip file name
-zip_file_name = "202211-citibike-tripdata.csv.zip"
-
-# create a ZipFile object
-zip_file = zipfile.ZipFile(zip_file_name)
-
-# extract all files from the zip file
-# the extractall() method will extract all the files to the current directory
-zip_file.extractall()
-
-# close the ZipFile object
-zip_file.close()
 
 
 
